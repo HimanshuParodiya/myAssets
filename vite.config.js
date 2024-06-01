ee6adcf04f749324c157131ce6e4e6b77e47032a
Himanshu <himanshuparodiya@gmail.com>
2024-06-01 09:31:39 +0530

    Add proxy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000" // where every find /api add "http://localhost:8000" before that so it will become "http://localhost:8000/api...."
+      // using proxy also help use to tell our cross-origin that this our url to now we will not get an error of cross-origin
+    }
+  },
   plugins: [react()],
 })
