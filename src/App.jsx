e679cead61ac33e1af5e749c3bed7fd6ebdced92
Himanshu <himanshuparodiya@gmail.com>
2024-06-01 16:26:19 +0530

    Add Navbar and react-dom-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="container app-container">
+      <Router>
+        <div className="side-bar">
+          <Navbar />
+        </div>
+        <Routes>
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route> */}
+        </Routes>
+      </Router>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="container nav-container">
+      <div className="hamburger-icon">
+        <span className="hamburger-icon-bar"></span>
+        <span className="hamburger-icon-bar"></span>
+        <span className="hamburger-icon-bar"></span>
+      </div>
+      <div className="nav-menus">
+        <ul className="nav-menu-ul">
+          <li className="nav-menu-li">
+            <Link className="" to={"/"}>
+              Dashboard
+            </Link>
+          </li>
+          <li className="nav-menu-li">
+            <Link className="" to={"/assets"}>
+              Assets
+            </Link>
+          </li>
+          <li className="nav-menu-li">
+            <Link className="" to={"/maintenance"}>
+              Maintenance
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
